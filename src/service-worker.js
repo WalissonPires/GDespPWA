63638a720c539a37eec8d5e500da83d401fe7e3f
Walisson Pires Rodrigues Miranda <walissonpiresgv@hotmail.com>
2019-04-21 14:00:28 -0300

    Correção do manifest

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var dataCacheName  = 'GDespDataCache';
-var cacheName = 'GDespShellCache-v1.0.5';
+var cacheName = 'GDespShellCache-v1.0.6';
 var filesToCache = [
     './',
     './index.html',
