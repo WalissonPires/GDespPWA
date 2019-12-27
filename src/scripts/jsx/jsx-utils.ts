f8b58eefb4b0becaae8cbdad690c0516c3aea0f9
Walisson Pires Rodrigues Miranda <walissonpiresgv@hotmail.com>
2019-12-27 16:26:12 -0300

    Add typescript support;

---
+++ src/scripts/jsx/jsx-utils.ts
@@ -0,0 +1,10 @@
+﻿
+export function jsxRaw(html: string, wrapperTag: string = 'div') {
+
+    let $el = $(html);
+
+    if ($el.length == 2)
+        $el = $(`<${wrapperTag}></${wrapperTag}>`).append($el);
+
+    return $el[0];
+}
---
+++ src/scripts/teste.tsx
@@ -0,0 +1,7 @@
+/// <reference path="jsx/jsx-factory.ts" />
+
+function getElement() {
+
+    const el = (<div>Teste</div>);
+    return el;
+}
